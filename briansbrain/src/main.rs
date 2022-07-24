87749d42c5f8296526b5c46c3a38a09927af4de6
Fady Adal <2masadel@gmail.com>
2022-07-25 00:13:59 +0200

    gui is now an optional feature

--- briansbrain/src/main.rs
+++ briansbrain/src/main.rs
@@ -1,8 +1,15 @@
 mod game;
+#[cfg(feature = "gui")]
 mod gui;
 
+#[cfg(feature = "gui")]
 fn main() -> Result<(), String> {
     gui::run()?;
 
+    Ok(())
+}
+
+#[cfg(not(feature = "gui"))]
+fn main() -> Result<(), String> {
     Ok(())
 }
--- gameoflife/src/main.rs
+++ gameoflife/src/main.rs
@@ -1,14 +1,22 @@
 mod game;
+#[cfg(feature = "gui")]
 mod gui;
 mod terminal;
 
+#[cfg(feature = "gui")]
 fn main() -> Result<(), String> {
     let args: Vec<_> = std::env::args().skip(1).collect();
-    if args.len() > 1 && args[0] == "in_terminal" {
+    if args.len() >= 1 && args[0] == "in_terminal" {
         terminal::run()?;
         Ok(())
     } else {
         gui::run()?;
         Ok(())
     }
+}
+
+#[cfg(not(feature = "gui"))]
+fn main() -> Result<(), String> {
+    terminal::run()?;
+    Ok(())
 }
