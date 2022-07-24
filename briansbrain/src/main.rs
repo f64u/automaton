151c06c18ce8daa6ed8f4023ae332794e5a52d80
Fady Adal <2masadel@gmail.com>
2022-07-25 00:19:44 +0200

    propagate optional feature to spaces and rename feature to sdl2

--- briansbrain/src/main.rs
+++ briansbrain/src/main.rs
@@ -1,15 +1,15 @@
 mod game;
-#[cfg(feature = "gui")]
+#[cfg(feature = "sdl2")]
 mod gui;
 
-#[cfg(feature = "gui")]
+#[cfg(feature = "sdl2")]
 fn main() -> Result<(), String> {
     gui::run()?;
 
     Ok(())
 }
 
-#[cfg(not(feature = "gui"))]
+#[cfg(not(feature = "sdl2"))]
 fn main() -> Result<(), String> {
     Ok(())
 }
--- gameoflife/src/main.rs
+++ gameoflife/src/main.rs
@@ -1,9 +1,9 @@
 mod game;
-#[cfg(feature = "gui")]
+#[cfg(feature = "sdl2")]
 mod gui;
 mod terminal;
 
-#[cfg(feature = "gui")]
+#[cfg(feature = "sdl2")]
 fn main() -> Result<(), String> {
     let args: Vec<_> = std::env::args().skip(1).collect();
     if args.len() >= 1 && args[0] == "in_terminal" {
@@ -15,7 +15,7 @@ fn main() -> Result<(), String> {
     }
 }
 
-#[cfg(not(feature = "gui"))]
+#[cfg(not(feature = "sdl2"))]
 fn main() -> Result<(), String> {
     terminal::run()?;
     Ok(())
